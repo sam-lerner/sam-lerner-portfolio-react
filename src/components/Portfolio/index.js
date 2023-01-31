4d16c8d0b0c1f2fe10687431a38a307dd532d64a
Sam Lerner <sam.a.lerner@gmail.com>
2023-01-31 17:22:33 -0500

    portfolio styled, need photos

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import PortfoioCard from '../PortfolioCard';
+import projects from './projects.json';
+
+const styles = {
+  header: {
+      color: "white",
+      textAlign: "center"
+  },
+}
 
 export default function Portfolio() {
   return (
-    <div>
-      <h1>Portfolio Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+    <div style={styles.header} >
+      <h1>Recent Projects</h1>
+      {projects.map((element) => {
+        
+        return <PortfoioCard project={element}></PortfoioCard>
+      })}
     </div>
   );
 }
