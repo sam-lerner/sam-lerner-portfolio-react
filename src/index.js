3b9e6bc0cf3babc29db20fae1fb9729607086233
Sam Lerner <sam.a.lerner@gmail.com>
2023-02-01 14:56:28 -0500

    Restored to end of Tuesday

--- src/index.js
+++ src/index.js
@@ -2,14 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 import App from './App';
-<<<<<<< HEAD
+
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 6f1bb76 (debugging merge issues)
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> e0f7d6b (File structure underway. Still trying to understand paths.)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
